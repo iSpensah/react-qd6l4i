55e27385f66142ddb25ffb652856c80a01d3236f
iSpensah <crazymeet1998@gmail.com>
2022-07-21 15:39:53 +0800

    1 file modified

--- src/ Login.js
+++ src/ Login.js
@@ -11,10 +11,12 @@ class Login extends React.Component {
   render() {
     return (
     <div>
-
+      <form>
       <input type ='text' placeholeder = 'Enter Username' value = {this.state.name}></input>
       <input type ='text' placeholeder = 'Enter Username' value = {this.state.name}></input>
       <input type ='submit' placeholeder = 'Enter Username' value = {this.state.name}></input>
+      
+      </form>
       </div>
       );
   }
